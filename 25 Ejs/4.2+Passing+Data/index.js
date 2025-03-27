bea46401e62c53d15677309ea1a7b70397e6f43f
Abjal Momin <afzalmomin818@gmail.com>
2025-03-27 17:05:17 +0530

    added 4.1 & 4.2

---
+++ 25 Ejs/4.2+Passing+Data/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const fName = req.body["fName"];
+  const lName = req.body["lName"];
+  const nameLength = fName.length + lName.length;
+  res.render("index.ejs", {
+    numberOfLetters: nameLength,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
